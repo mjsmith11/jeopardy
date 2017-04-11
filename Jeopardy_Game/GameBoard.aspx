6ca8c647a8ea8b2ed055e479ed5de42910974f36
Matt Smith <mjsmith92@outlook.com>
2017-04-10 22:40:01 -0400

    Beginnings of a gameboard

---
+++ Jeopardy_Game/GameBoard.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GameBoard.aspx.cs" Inherits="Jeopardy_Game.GameBoard" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Table ID="tblGameboard" runat="server">
+        </asp:Table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Jeopardy_Game/GameBoard.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Jeopardy_Game
+{
+    public partial class GameBoard : System.Web.UI.Page
+    {
+        private int[] values = { 100, 200, 300, 400, 500 };
+        private const int NUM_CATEGORIES = 6;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                TableRow Header = new TableRow();
+                tblGameboard.Rows.Add(Header);
+                for(int i=1; i<=NUM_CATEGORIES; i++)
+                {
+                    TableCell cell = new TableCell();
+                    cell.Text = "category" + i;
+                    Header.Cells.Add(cell);
+                }
+                foreach(int value in values)
+                {
+                    TableRow row = new TableRow();
+                    tblGameboard.Rows.Add(row);
+                    for(int i=0; i<NUM_CATEGORIES; i++)
+                    {
+                        TableCell cell = new TableCell();
+                        cell.Text = value.ToString();
+                        row.Cells.Add(cell);
+                    }
+                }                
+            }
+        }
+    }
+}
